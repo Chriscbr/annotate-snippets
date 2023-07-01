a4144999619b0d78278d06f51d1985461f54b4cb
Christopher Rybicki <crybicki98@gmail.com>
2023-07-01 18:53:07 -0400

    add pkg to source control

---
+++ pkg/index.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {any} title
+* @param {any} footer
+* @param {any} slices
+* @param {any} options
+* @returns {string}
+*/
+export function annotate_snippet(title: any, footer: any, slices: any, options: any): string;
